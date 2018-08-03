4f8e34b80f0b7a807494bd6efb71717f7d26f8fc
Ibrahim Sassi <isassi@ats-digital.com>
2018-08-03 09:42:01 +0100

    clearing LS after logout

--- src/UIBundle/Resources/public/dev/app/scripts/controllers/login.js
+++ src/UIBundle/Resources/public/dev/app/scripts/controllers/login.js
@@ -2,9 +2,9 @@
  * Created by hamed on 02/06/17.
  */
 
-angular.module('leadwireApp')
-    .controller('LoginCtrl', LoginController)
-    .controller('logoutCtrl', logout);
+angular.module('leadwireApp').
+    controller('LoginCtrl', LoginController).
+    controller('logoutCtrl', logout);
 
 function logout($location, $localStorage) {
     $localStorage.$reset();
@@ -22,42 +22,41 @@ LoginController.$inject = ['$location', '$auth', '$timeout', 'User'];
  * @param User
  * @constructor
  */
-function LoginController($location, $auth, $timeout, User) {
+function LoginController($location, $auth, $timeout, User, $localStorage) {
     var vm = this;
 
     vm.authenticate = authenticate;
 
     (function initController() {
         // reset login status
-        if (!$auth.isAuthenticated()) { return; }
-        $auth.logout()
-            .then(function () {
-                // toastr.info('You have been logged out');
-                $location.path('/');
-            });
+        if (!$auth.isAuthenticated()) {
+            return;
+        }
+        $auth.logout().then(function() {
+            // toastr.info('You have been logged out');
+            $localStorage.user = null;
+            $location.path('/');
+        });
     })();
-
 
     function authenticate(provider) {
         vm.dataLoading = true;
-        $auth.authenticate(provider)
-            .then(function () {
-                // toastr.success('You have successfully signed in with ' + provider + '!');
-                User.getProfile();
-                $timeout(function () {
-                    vm.dataLoading = false;
-                    $location.path('/');
-                }, 200);
-            })
-            .catch(function (error) {
+        $auth.authenticate(provider).then(function() {
+            // toastr.success('You have successfully signed in with ' + provider + '!');
+            User.getProfile();
+            $timeout(function() {
                 vm.dataLoading = false;
-                if (error.message) {
-                    // toastr.error(error.message);
-                } else if (error.data) {
-                    // toastr.error(error.data.message, error.status);
-                } else {
-                    //toastr.error(error);
-                }
-            });
+                $location.path('/');
+            }, 200);
+        }).catch(function(error) {
+            vm.dataLoading = false;
+            if (error.message) {
+                // toastr.error(error.message);
+            } else if (error.data) {
+                // toastr.error(error.data.message, error.status);
+            } else {
+                //toastr.error(error);
+            }
+        });
     };
 }
