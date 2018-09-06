040a970978f4713d6b8801c45add9e105dd7fccf
Anis Ksontini <aksontini@ats-digital.com>
2018-09-06 22:38:02 +0100

    Handle Cancel Subscription.

--- src/ATS/PaymentBundle/Service/PaymentService.php
+++ src/ATS/PaymentBundle/Service/PaymentService.php
@@ -3,12 +3,9 @@
 namespace ATS\PaymentBundle\Service;
 
 use ATS\PaymentBundle\Exception\OmnipayException;
-use Monolog\Logger;
 use ATS\PaymentBundle\Service\GateWay;
 use Omnipay\Common\CreditCard;
 use ATS\PaymentBundle\Document\Customer;
-use ATS\PaymentBundle\Service\CustomerService;
-use ATS\PaymentBundle\Exception\CustomerNotRecognizedException;
 use Psr\Log\LoggerInterface;
 
 class PaymentService
@@ -27,16 +24,6 @@ class PaymentService
      * @var LoggerInterface
      */
     private $logger;
-
-    /**
-     * @var string
-     */
-    private $secretKey;
-
-    /**
-     * @var string
-     */
-    private $apiKey;
 
     /**
      * PaymentService constructor.
@@ -88,71 +75,6 @@ class PaymentService
             }
         } else {
             return false;
-        }
-    }
-
-    /**
-     * @param $subscriptionName
-     * @param Customer $customer
-     * @return string | bool
-     *
-     * @throws OmnipayException
-     */
-    public function createSubscription($subscriptionName, Customer $customer)
-    {
-        $response = $this->gateway->createSubscription([
-            'plan' => $subscriptionName,
-            "customerReference" => $customer->getGatewayToken(),
-        ])->send();
-
-        if (!$response->isSuccessful()) {
-            $this->logger->critical($response->getMessage());
-            throw new OmnipayException($response->getMessage());
-        } else {
-            return $response->getData()['id'];
-        }
-    }
-
-    /**
-     * @param string $sub
-     * @param Customer $customer
-     * @return array | bool
-     * @throws OmnipayException
-     */
-    public function fetchSubscription(string $sub, Customer $customer)
-    {
-        $response = $this->gateway->fetchSubscription(
-            ['subscriptionReference' => $sub, 'customerReference' =>  $customer->getGatewayToken()]
-        )->send();
-
-        if ($response->isSuccessful()) {
-            return $response->getData();
-        } else {
-            $this->logger->error($response->getMessage());
-            throw new OmnipayException($response->getMessage());
-        }
-    }
-
-    /**
-     * @param string $customerReference
-     * @param string $subscriptionReference
-     * @param string $planReference
-     * @return mixed
-     * @throws OmnipayException
-     */
-    public function updateSubscription(string $customerReference, string $subscriptionReference, string $planReference)
-    {
-        $response = $this->gateway->updateSubscription([
-            'customerReference' => $customerReference,
-            'subscriptionReference' => $subscriptionReference,
-            'plan' => $planReference
-        ])->send();
-
-        if ($response->isSuccessful()) {
-            return $response->getData();
-        } else {
-            $this->logger->error($response->getMessage());
-            throw new OmnipayException($response->getMessage());
         }
     }
 }
