07ca708dfe0a53bf81290d2b9945d44d25d4b2c5
Anis Ksontini <aksontini@ats-digital.com>
2018-09-14 15:50:03 +0100

    Fix Subscription Call

--- src/AppBundle/Service/UserService.php
+++ src/AppBundle/Service/UserService.php
@@ -197,8 +197,8 @@ class UserService
     {
         $plan = $this->planService->getPlan($data['plan']);
         $token = false;
-
         if ($plan) {
+            $anchorCycle = $user->getPlan()->getPrice() < $plan->getPrice() ? 'unchanged' : $data['periodEnd'];
             if ($plan->getPrice() == 0) {
                 $this->subscriptionService->delete(
                     $user->getSubscriptionId(),
@@ -215,7 +215,6 @@ class UserService
                 }
 
                 if (is_string($token)) {
-                    $anchorCycle = isset($data['periodEnd']) ? $data['periodEnd'] : time();
                     $data = $this->subscriptionService->update(
                         $user->getCustomer()->getGatewayToken(),
                         $user->getSubscriptionId(),
