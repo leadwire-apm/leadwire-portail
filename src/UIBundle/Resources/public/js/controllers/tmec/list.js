d2ab23296e384296ee961d2b61ae7b59c08eb863
hdhib <hdhib@dydu.ai>
2019-03-09 17:53:54 +0100

    test

--- src/UIBundle/Resources/public/js/controllers/tmec/list.js
+++ src/UIBundle/Resources/public/js/controllers/tmec/list.js
@@ -34,7 +34,9 @@
                 controller: 'StepCtrl',
                 controllerAs: 'ctrl',
                 resolve: {
-                    compagneId: id
+                    compagneId: function () {
+                        return id;
+                    }
                 }
             });
 
