c70f1732d1cb195cfe7aef831d786ee0b0632d58
hdhib <hdhib@dydu.ai>
2019-03-11 21:03:45 +0100

    test

--- src/UIBundle/Resources/public/js/controllers/tmec/overview.js
+++ src/UIBundle/Resources/public/js/controllers/tmec/overview.js
@@ -21,5 +21,48 @@
         $modal,
     ) {
         var vm = this;
+
+            /**
+     * Handle add new application logic
+     *
+     */
+    function ManageApplicationsCtrlFN (
+        ApplicationService,
+        CodeService,
+        toastr,
+        MESSAGES_CONSTANTS,
+        $state,
+    ) {
+        var vm = this;
+
+        vm.flipActivityIndicator = function (key) {
+            vm.ui[key] = !vm.ui[key];
+        };
+
+
+        vm.loadApplications = function () {
+            vm.flipActivityIndicator('isLoading');
+            // should send some criteria
+            ApplicationService.all()
+                .then(function (applications) {
+                    vm.flipActivityIndicator('isLoading');
+                    vm.applications = applications;
+                })
+                .catch(function (error) {
+                    vm.flipActivityIndicator('isLoading');
+                    vm.applications = [];
+                });
+        };
+
+        vm.init = function () {
+            vm = angular.extend(vm, {
+                ui: {
+                    isLoading: false
+                },
+                applications: [],
+            });
+            vm.loadApplications();
+        }
     }
+}
 })(window.angular);
