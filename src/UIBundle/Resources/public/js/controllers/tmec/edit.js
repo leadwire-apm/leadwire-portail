c32cc58dea47d344b1d669053d0f9d11c4480d2a
hdhib@dydu.ai <hdhib@dydu.ai>
2019-03-11 16:17:40 +0100

    steps

--- src/UIBundle/Resources/public/js/controllers/tmec/edit.js
+++ src/UIBundle/Resources/public/js/controllers/tmec/edit.js
@@ -43,7 +43,7 @@
                     vm.flipActivityIndicator('isSaving')
                     toastr.success(MESSAGES_CONSTANTS.SUCCESS);
                     $state.go('app.management.tmecs', {
-                        id: compagne.applicationId,
+                        id: vm.compagne.applicationId,
                     });
                 })
                 .catch(function () {
