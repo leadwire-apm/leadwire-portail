664d2c6649742fbbe6cbb18e9485b1243a436133
hdhib <hdhib@dydu.ai>
2019-03-12 01:35:02 +0100

    test

--- src/UIBundle/Resources/public/js/controllers/tmec/overview.js
+++ src/UIBundle/Resources/public/js/controllers/tmec/overview.js
@@ -101,4 +101,22 @@
             vm.loadApplications();
         }
     }
+
+    $(document).ready(function() {
+        //toggle the component with class accordion_body
+        $(".accordion_head").click(function() {
+          if ($('.accordion_body').is(':visible')) {
+            $(".accordion_body").slideUp(300);
+            $(".plusminus").text('+');
+          }
+          if ($(this).next(".accordion_body").is(':visible')) {
+            $(this).next(".accordion_body").slideUp(300);
+            $(this).children(".plusminus").text('+');
+          } else {
+            $(this).next(".accordion_body").slideDown(300);
+            $(this).children(".plusminus").text('-');
+          }
+        });
+      });
+      
 })(window.angular);
