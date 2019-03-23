67fe2dde3b2f8c75ceca8430538027d8dedb6de5
hdhib <hdhib@dydu.ai>
2019-03-23 22:02:08 +0100

    steps

--- src/AppBundle/Service/StepService.php
+++ src/AppBundle/Service/StepService.php
@@ -9,58 +9,7 @@ use AppBundle\Manager\StepManager;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-$stepsList = [
-    {
-        "label": "Cadrage",
-        "order": 1,
-        "current": true
-    },
-    {
-        "label": "Devis",
-        "order": 2,
-        "current": false
-    },
-    {
-        "label": "CDC",
-        "order": 3,
-        "current": false
-    },
-    {
-        "label": "R7J",
-        "order": 4,
-        "current": false
-    },
-    {
-        "label": "Scipts Jdd",
-        "order": 5,
-        "current": false
-    },
-    {
-        "label": "PP",
-        "order": 6,
-        "current": false
-    },
-    {
-        "label": "Outils Tperf",
-        "order": 7,
-        "current": false
-    },
-    {
-        "label": "Tuning",
-        "order": 8,
-        "current": false
-    },
-    {
-        "label": "Ref",
-        "order": 9,
-        "current": false
-    },
-    {
-        "label": "Rapport",
-        "order": 10,
-        "current": false
-    }
-]
+
 
 class StepService
 {
@@ -87,6 +36,59 @@ class StepService
      */
     public function initSteps(Tmec $tmec)
     {
+        $stepsList = [
+            {
+                "label": "Cadrage",
+                "order": 1,
+                "current": true
+            },
+            {
+                "label": "Devis",
+                "order": 2,
+                "current": false
+            },
+            {
+                "label": "CDC",
+                "order": 3,
+                "current": false
+            },
+            {
+                "label": "R7J",
+                "order": 4,
+                "current": false
+            },
+            {
+                "label": "Scipts Jdd",
+                "order": 5,
+                "current": false
+            },
+            {
+                "label": "PP",
+                "order": 6,
+                "current": false
+            },
+            {
+                "label": "Outils Tperf",
+                "order": 7,
+                "current": false
+            },
+            {
+                "label": "Tuning",
+                "order": 8,
+                "current": false
+            },
+            {
+                "label": "Ref",
+                "order": 9,
+                "current": false
+            },
+            {
+                "label": "Rapport",
+                "order": 10,
+                "current": false
+            }
+        ];
+        
         foreach($stepsList as $step)
         {
             $this->stepManager->create($tmec, $step["label"], $step["order"], $step["current"]);
