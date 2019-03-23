80954ed27350b23ce5d014fd3a05e0ff32ed8bd4
hdhib <hdhib@dydu.ai>
2019-03-23 21:27:57 +0100

    steps

--- src/UIBundle/Resources/public/js/controllers/mainApp.js
+++ src/UIBundle/Resources/public/js/controllers/mainApp.js
@@ -38,6 +38,7 @@
         onLoad();
 
         $scope.JENKINS_URL = CONFIG.JENKINS_URL;
+        $scope.COMPAGNE_ENABLED = config.COMPAGNE_ENABLED;
         
         $scope.$on('user:updated', function (event, data) {
             $rootScope.user = data;
