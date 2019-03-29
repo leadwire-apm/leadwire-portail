53d2bea7f39e174bb170f8a12f19c5f78953f60a
hdhib <hdhib@dydu.ai>
2019-03-29 15:21:04 +0100

    rectifiction import

--- src/AppBundle/Service/StepService.php
+++ src/AppBundle/Service/StepService.php
@@ -7,7 +7,6 @@ use AppBundle\Manager\StepManager;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Finder\Finder;
 
 class StepService
 {
