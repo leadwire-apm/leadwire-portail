206196292654e95f7510926906ff074d8b3156a7
hdhib <hdhib@dydu.ai>
2019-04-19 00:53:09 +0200

    fix date picker bug

--- src/UIBundle/Resources/public/js/controllers/tmec/add.js
+++ src/UIBundle/Resources/public/js/controllers/tmec/add.js
@@ -8,7 +8,102 @@
             '$stateParams',
             'ApplicationFactory',
             AddCompagnesCtrlFN,
-        ]);
+        ])    
+        .directive('myDatePicker', function () {
+            return {
+                restrict: 'A',
+                require: '?ngModel',
+                link: function (scope, element, attrs, ngModelController) {
+          
+                    // Private variables
+                    var datepickerFormat = 'm/d/yyyy',
+                        momentFormat = 'M/D/YYYY',
+                        datepicker,
+                        elPicker;
+          
+                    // Init date picker and get objects http://bootstrap-datepicker.readthedocs.org/en/release/index.html
+                    datepicker = element.datepicker({
+                        autoclose: true,
+                        keyboardNavigation: false,
+                        todayHighlight: true,
+                        format: datepickerFormat
+                    });
+                    elPicker = datepicker.data('datepicker').picker;
+          
+                    // Adjust offset on show
+                    datepicker.on('show', function (evt) {
+                        elPicker.css('left', parseInt(elPicker.css('left')) + +attrs.offsetX);
+                        elPicker.css('top', parseInt(elPicker.css('top')) + +attrs.offsetY);
+                    });
+          
+                    // Only watch and format if ng-model is present https://docs.angularjs.org/api/ng/type/ngModel.NgModelController
+                    if (ngModelController) {
+                        // So we can maintain time
+                        var lastModelValueMoment;
+          
+                        ngModelController.$formatters.push(function (modelValue) {
+                            //
+                            // Date -> String
+                            //
+          
+                            // Get view value (String) from model value (Date)
+                            var viewValue,
+                                m = moment(modelValue);
+                            if (modelValue && m.isValid()) {
+                                // Valid date obj in model
+                                lastModelValueMoment = m.clone(); // Save date (so we can restore time later)
+                                viewValue = m.format(momentFormat);
+                            } else {
+                                // Invalid date obj in model
+                                lastModelValueMoment = undefined;
+                                viewValue = undefined;
+                            }
+          
+                            // Update picker
+                            element.datepicker('update', viewValue);
+          
+                            // Update view
+                            return viewValue;
+                        });
+          
+                        ngModelController.$parsers.push(function (viewValue) {
+                            //
+                            // String -> Date
+                            //
+          
+                            // Get model value (Date) from view value (String)
+                            var modelValue,
+                                m = moment(viewValue, momentFormat, true);
+                            if (viewValue && m.isValid()) {
+                                // Valid date string in view
+                                if (lastModelValueMoment) { // Restore time
+                                    m.hour(lastModelValueMoment.hour());
+                                    m.minute(lastModelValueMoment.minute());
+                                    m.second(lastModelValueMoment.second());
+                                    m.millisecond(lastModelValueMoment.millisecond());
+                                }
+                                modelValue = m.toDate();
+                            } else {
+                                // Invalid date string in view
+                                modelValue = undefined;
+                            }
+          
+                            // Update model
+                            return modelValue;
+                        });
+          
+                        datepicker.on('changeDate', function (evt) {
+                            // Only update if it's NOT an <input> (if it's an <input> the datepicker plugin trys to cast the val to a Date)
+                            if (evt.target.tagName !== 'INPUT') {
+                                ngModelController.$setViewValue(moment(evt.date).format(momentFormat)); // $seViewValue basically calls the $parser above so we need to pass a string date value in
+                                ngModelController.$render();
+                            }
+                        });
+                    }
+          
+                }
+            };
+        });
 
     /**
      * Handle add new compagnes logic
