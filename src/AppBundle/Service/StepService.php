3b66061a7a3cbe3264107f2bbbe6d94116131e7c
Lead Wire <wassimdhib@yahoo.fr>
2019-05-10 14:46:47 +0200

    small fix

--- src/AppBundle/Service/StepService.php
+++ src/AppBundle/Service/StepService.php
@@ -33,7 +33,7 @@ class StepService
         $this->stepManager->create($tmec, "Devis", 2, false);
         $this->stepManager->create($tmec, "CDC", 3, false);
         $this->stepManager->create($tmec, "R7J", 4, false);
-        $this->stepManager->create($tmec, "Scipts Jdd", 5, false);
+        $this->stepManager->create($tmec, "Scripts Jdd", 5, false);
         $this->stepManager->create($tmec, "PP", 6, false);
         $this->stepManager->create($tmec, "Outils Tperf", 7, false);
         $this->stepManager->create($tmec, "Tuning", 8, false);
