b7deb57f75440c1f9c8f3e8aa80e75c90745feee
Wajih WERIEMI <wweriemi@ats-digital.com>
2019-11-11 11:28:41 +0100

    clean code to resize iframe

--- src/UIBundle/Resources/public/js/directives/iframe.js
+++ src/UIBundle/Resources/public/js/directives/iframe.js
@@ -3,32 +3,15 @@
 /*
  * vector - jvectormap directive
  */
-function iframeSetDimensionsOnload() {
+function iframeSetDimensionsOnload(iFrameService) {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
             element.on('load', function() {
-                setElementDimension(element);
+                iFrameService.setDimensions(element);
             })
         }
     };
 }
-
-
-function setElementDimension(element) {
-    $('body').addClass('iframe');
-    $('.main-content').css('margin', '-15px 0px 0px 0px');
-    $('.main-content').css('padding', '80px 0px 0px 0px');
-    var height = window.innerHeight - 120;
-    var iFrameHeight = height + 'px'
-    var iFrameWidth = '100%';
-    element.css('width', iFrameWidth);
-    element.css('height', iFrameHeight);
-}
-
-$(window).on('resize', function() {
-    console.log('here');
-    setElementDimension($('iframe'));
-});
 
 angular.module('leadwireApp').directive('iframeSetDimensionsOnload', iframeSetDimensionsOnload);
