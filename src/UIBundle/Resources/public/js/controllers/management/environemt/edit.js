8c8aaa460d5dae9abcf574dfa85079ef1b60bc0b
hameddhib <hameddhib@hotmail.com>
2019-11-19 12:08:14 +0000

    ticket-39 add js file

--- src/UIBundle/Resources/public/js/controllers/management/environemt/edit.js
+++ src/UIBundle/Resources/public/js/controllers/management/environemt/edit.js
@@ -2,22 +2,18 @@
     angular.module('leadwireApp')
         .controller('EditEnvironmentController', [
             'EnvironmentService',
-            'MonitoringSetService',
-            '$stateParams',
             'MESSAGES_CONSTANTS',
             '$state',
             'toastr',
-            EditApplicationTypeControllerCtrlFN,
+            EditEnvironmentControllerCtrlFN,
         ]);
 
     /**
      * Handle add new application logic
      *
      */
-    function EditApplicationTypeControllerCtrlFN (
-        ApplicationTypeService,
-        MonitoringSetService,
-        $stateParams,
+    function EditEnvironmentControllerCtrlFN (
+        EnvironmentService,
         MESSAGES_CONSTANTS,
         $state,
         toastr,
@@ -28,39 +24,13 @@
             vm.ui[key] = !vm.ui[key];
         };
 
-        vm.loadApplicationType = function (id) {
-            ApplicationTypeService.find(id)
-                .then(function (appType) {
-                    selected = [];
-                    vm.applicationType = appType;
-                    vm.applicationType.monitoringSets
-                    .forEach(function(ms) {
-                        selected.push(ms.id);
-                    });
-                    $('.selectpicker').selectpicker('val', selected);
-                    $('.selectpicker').selectpicker('refresh');
-                });
-
-        };
-
-        vm.loadMonitoringSets = function() {
-            MonitoringSetService.listValid()
-            .then(function(monitoringSets) {
-                vm.availableMonitoringSets = monitoringSets;
-                $('.selectpicker').append(vm.availableMonitoringSets.map(function(v,k){return '<option value="' + v.id + '">'+v.name+'</option>'}));
-                $('.selectpicker').selectpicker('refresh');
-                vm.loadApplicationType($stateParams.id)
-            });
-        };
-
-        vm.editAppType = function () {
+        vm.editEnvironment = function () {
             vm.flipActivityIndicator('isSaving')
-            vm.applicationType.monitoringSets = vm.applicationType.monitoringSets.map(function (ms) {return {'id': ms};});
-            ApplicationTypeService.update(vm.applicationType)
+            EnvironmentService.update(vm.environment)
                 .then(function () {
                     vm.flipActivityIndicator('isSaving')
                     toastr.success(MESSAGES_CONSTANTS.SUCCESS);
-                    $state.go('app.management.applicationTypes');
+                    $state.go('app.management.environmentList');
                 })
                 .catch(function () {
                     vm.flipActivityIndicator('isSaving')
@@ -74,15 +44,10 @@
                     isSaving: false,
                     isLoading: false,
                 },
-                applicationType: {
-                    name: '',
-                    description: '',
-                    installation: '',
-                    monitoringSets:[]
-                },
-                availableMonitoringSets: []
+                environment: {
+                    name: ''
+                }
             });
-            vm.loadMonitoringSets();
         };
 
     }
