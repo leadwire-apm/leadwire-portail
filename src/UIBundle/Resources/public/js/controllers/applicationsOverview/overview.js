eb74bb931fdb6b71393b71d14e73b0efc253c9b4
hameddhib <hameddhib@hotmail.com>
2020-01-03 19:36:31 +0000

    fix css

--- src/UIBundle/Resources/public/js/controllers/applicationsOverview/overview.js
+++ src/UIBundle/Resources/public/js/controllers/applicationsOverview/overview.js
@@ -1,24 +1,51 @@
 (function (angular) {
     angular.module('leadwireApp')
-        .controller('ApplicationOverviewController', ['ApplicationFactory','ApplicationService', ApplicationsOverviewCtrlFN]);
+        .controller('ApplicationOverviewController', ['ApplicationFactory', 'ApplicationService', ApplicationsOverviewCtrlFN]);
 
     /**
      * Handle Applications stats
      *
      */
     function ApplicationsOverviewCtrlFN(ApplicationFactory, ApplicationService) {
-        
+
         var vm = this;
+
+        var SI_SYMBOL = [" Transactions", 
+                           " k Transactions", 
+                           " M Transactions", 
+                           " G Transactions", 
+                           " T Transactions", 
+                           " P Transactions", 
+                           " E Transactions"];
+
+        vm.abbreviateNumber = function (number) {
+
+            // what tier? (determines SI symbol)
+            var tier = Math.log10(number) / 3 | 0;
+
+            // if zero, we don't need a suffix
+            if (tier == 0) return  " 0 Transactions";
+
+            // get suffix and determine scale
+            var suffix = SI_SYMBOL[tier];
+            var scale = Math.pow(10, tier * 3);
+
+            // scale the number
+            var scaled = number / scale;
+
+            // format number and add suffix
+            return (scaled.toFixed(1) + suffix);
+        }
 
         vm.load = function () {
             ApplicationFactory.findMyApplications().then(function (response) {
                 vm.applications = response.data;
                 vm.applications.forEach(application => {
-                    ApplicationService.getApplicationDocumentsCount(application.name).then(function (response){
-                        application.count = response.count;
+                    ApplicationService.getApplicationDocumentsCount(application.name).then(function (response) {
+                        application.count = vm.abbreviateNumber(response.count);
                     })
                 });
-                
+
             }).catch(function () {
             });
         };
@@ -29,7 +56,7 @@
                     isSaving: false,
                     isLoading: false,
                 },
-                applications :[]
+                applications: []
             });
             vm.load();
         };
