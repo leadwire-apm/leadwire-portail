cd0613a51005946f032462ec88d313c028e47d30
WassimDhib <wassimdhib@gmail.com>
2020-01-28 21:49:11 +0000

    fix index template for apm

--- src/AppBundle/Document/Template.php
+++ src/AppBundle/Document/Template.php
@@ -15,7 +15,7 @@ use JMS\Serializer\Annotation as JMS;
  */
 class Template
 {
-    const DEFAULT_VERSION = "6.5.1";
+    const DEFAULT_VERSION = "7.3.2";
     const DASHBOARDS = "Dashboards";
     const INDEX_TEMPLATE = "Index-Template";
     const INDEX_PATTERN = "Index-Pattern";
