aeb613463831ff19dce59fc85ec6d3efc3e0e642
hameddhib <hameddhib@hotmail.com>
2020-04-17 12:41:21 +0000

    Application#create Watcher #124

--- src/UIBundle/Resources/public/js/controllers/watcher/add.js
+++ src/UIBundle/Resources/public/js/controllers/watcher/add.js
@@ -18,5 +18,30 @@
         vm.cancel = function () {
             $modalInstance.dismiss();
         }
+
+        vm.startDate;
+        vm.endDate;
+
+        vm.options = {
+            locale: { cancelLabel: 'Clear' },
+            showDropdowns: true,
+            singleDatePicker: false,
+            alwaysShowCalendars: true,
+            showCustomRangeLabel: false,
+            linkedCalendars: false,
+            minDate: "01/01/2010",
+            //maxDate: moment(),
+            timePicker: true,
+            timePicker24Hour: true,
+            ranges: {
+                'Last 15 minutes': [moment().subtract(15, 'minutes'), moment()],
+                'Last 30 minutes': [moment().subtract(30, 'minutes'), moment()],
+                'Today': [moment(), moment()],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'Last Year': [moment().subtract(12, 'month'), moment()]
+
+            }
+        };
     }
 })(window.angular);
